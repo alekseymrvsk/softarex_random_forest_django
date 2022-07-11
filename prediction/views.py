3f4be71a3cdf0946e8d3028a0917932c6f2f8313
Aleksey <alekseymrvsk@yandex.by>
2022-07-11 12:44:51 +0300

    commit

--- prediction/views.py
+++ prediction/views.py
@@ -2,26 +2,44 @@ from django.shortcuts import render, redirect
 import requests
 from .classRandomForest import MyRandomForest
 from .form import DocumentForm
+from .models import Files
 
 
-def model_form_upload(request):
+# def model_form_upload(request):
+#     if request.method == 'POST':
+#         form = DocumentForm(request.POST, request.FILES)
+#         if form.is_valid():
+#             form.save()
+#             file = Files.objects.last()
+#             model = MyRandomForest()
+#             model.fit_model()
+#             predict = model.predict_data(file)
+#             metric = model.get_metric()
+#             form = DocumentForm(request.POST, request.FILES)
+#             context = {'metric': metric, 'predict': predict, 'form': form}
+#             render(request, 'prediction/prediction.html', context)
+#     else:
+#         form = DocumentForm()
+#     return render(request, 'prediction/prediction.html', {
+#         'form': form
+#     })
+
+
+def index(request):
     if request.method == 'POST':
         form = DocumentForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
-            return redirect('home')
+            file = Files.objects.last()
+            model = MyRandomForest()
+            model.fit_model()
+            predict = model.predict_data(file)
+            metric = model.get_metric()
+            form = DocumentForm(request.POST, request.FILES)
+            context = {'metric': metric, 'predict': predict, 'form': form}
+            render(request, 'prediction/prediction.html', context)
     else:
         form = DocumentForm()
     return render(request, 'prediction/prediction.html', {
         'form': form
     })
-
-
-def index(request):
-    model = MyRandomForest()
-    model.fit_model()
-    predict = model.predict_data()
-    metric = model.get_metric()
-    form = DocumentForm(request.POST, request.FILES)
-    context = {'metric': metric, 'predict': predict, 'form': form}
-    return render(request, 'prediction/prediction.html', context)
