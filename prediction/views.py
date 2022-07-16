89be136d74fee3951813d37f033ac83eba2f1b6c
Aleksey <alekseymrvsk@yandex.by>
2022-07-16 13:46:18 +0300

    commit

--- prediction/views.py
+++ prediction/views.py
@@ -3,19 +3,33 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from reportlab.pdfgen import canvas
 from .classRandomForest import MyRandomForest
+import logging
+logger = logging.getLogger(__name__)
 
 
 def index(request):
+    model = MyRandomForest()
     if request.method == "POST":
-        uploaded_file = request.FILES.get('file_to_process')
+        if request.FILES.get('file_to_train') is not None:
+            uploaded_file_train = request.FILES.get('file_to_train')
+            fs = FileSystemStorage()
+            filename = fs.save(uploaded_file_train.name, uploaded_file_train)
+            uploaded_file_train_url = fs.url(filename)
+            model.fit_model(uploaded_file_train_url[1:])
+            logger.error("Train model using user data")
+        else:
+            model.fit_model()
+            logger.error("Train model using default data")
+
+        uploaded_file_predict = request.FILES.get('file_to_process')
         fs = FileSystemStorage()
-        filename = fs.save(uploaded_file.name, uploaded_file)
+        filename = fs.save(uploaded_file_predict.name, uploaded_file_predict)
         uploaded_file_url = fs.url(filename)
 
-        model = MyRandomForest()
-        model.fit_model()
         predict = model.predict_data(uploaded_file_url[1:])
+        logger.error("Predict data")
         metric = model.get_metric()
+        logger.error("Get metric")
 
         context = {'metric': metric, 'predict': predict, }
         return render(request, 'prediction/prediction.html', context)
--- users/views.py
+++ users/views.py
@@ -3,6 +3,8 @@ from django.urls import reverse_lazy
 from django.contrib.auth.forms import UserCreationForm
 from django.views.generic.edit import CreateView
 from django.shortcuts import redirect
+import logging
+logger = logging.getLogger(__name__)
 
 
 def home(request):
@@ -18,3 +20,4 @@ class SignUp(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
+    logger.error("Sign up")
