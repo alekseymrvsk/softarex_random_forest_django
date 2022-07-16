8bb1e81346bf208890b8d945b338e68212d5e789
Aleksey <alekseymrvsk@yandex.by>
2022-07-16 16:53:07 +0300

    commit

--- users/models.py
+++ users/models.py
@@ -4,11 +4,10 @@ from django.contrib.auth.models import User
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    n_predict = models.ImageField()
+    n_predict = models.IntegerField(default=0)
 
     def __unicode__(self):
         return self.user
 
-    class Meta:
-        verbose_name = 'Profile'
-        verbose_name_plural = 'Profiles'
+    def __str__(self):
+        return str(self.user)
--- users/views.py
+++ users/views.py
@@ -4,11 +4,39 @@ from django.contrib.auth.forms import UserCreationForm
 from django.views.generic.edit import CreateView
 from django.shortcuts import redirect
 import logging
+from django.shortcuts import render, redirect
+from datetime import datetime
+from .forms import (EditProfileForm, ProfileForm)
+from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.decorators import login_required
+
 logger = logging.getLogger(__name__)
 
 
 def home(request):
     return render(request, "registration/home.html")
+
+
+def profile(request):
+    return render(request, "registration/user_profile.html")
+
+
+@login_required
+def edit_profile(request):
+    if request.method == 'POST':
+        form = EditProfileForm(request.POST, instance=request.user)
+
+        if form.is_valid():
+            user_form = form.save()
+            custom_form = form.save(False)
+            custom_form.user = user_form
+            custom_form.save()
+            return redirect('/user_profile/')
+    else:
+        form = EditProfileForm(instance=request.user)
+
+        context = {'form': form}
+        return render(request, 'registration/edit_profile.html', context)
 
 
 def redirect_view(request):
